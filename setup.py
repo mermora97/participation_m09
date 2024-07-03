678d76b5252f71328742f045ae682c46f0700fdd
Mercedes M <mermora97@gmail.com>
2024-07-03 12:40:08 -0400

    added mymodule package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Test',
+    version='1.0.0',
+    url='https://github.com/mermora97/participation_m09',
+    author='Mercedes Mora-Figueroa',
+    author_email='eqa7yg@virginia.edu',
+    description='Description of my package',
+    license='MIT',
+    packages=['packagedir']    
+)
